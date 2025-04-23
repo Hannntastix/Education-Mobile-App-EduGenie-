836a79e63c838f5482d3d49a6854c568df42b927
M.Raihan Athalah Ilham <162966950+Hannntastix@users.noreply.github.com>
2025-04-23 23:09:40 +0700

    Update API key visibility

--- config/firebaseConfig.jsx
+++ config/firebaseConfig.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAY65Yp4SZX2wPGDfPczWUG-Tey1o4nsNc",
-  authDomain: "edugenie-42c60.firebaseapp.com",
-  projectId: "edugenie-42c60",
-  storageBucket: "edugenie-42c60.firebasestorage.app",
-  messagingSenderId: "435417886612",
-  appId: "1:435417886612:web:89439d3f1aa44bd0ee6981",
-  measurementId: "G-339PM1TV02"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
